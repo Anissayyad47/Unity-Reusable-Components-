f81c89088a665737eb2a32977a655db83a8bd488
ANIS SAYYAD <156597008+Anissayyad47@users.noreply.github.com>
2026-08-19 17:55:11 +0530

    Implemented interaction system in InteractionController

--- Components/Interaction System/InteractionController.cs
+++ Components/Interaction System/InteractionController.cs
@@ -1,1 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class InteractionController : MonoBehaviour
+{
+    private IInteractable _IInteractable;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Interact();
+        }
+    }
+    
+    private void Interact()
+    {
+        if(_IInteractable == null) return;
+        _IInteractable.Interact();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _IInteractable = other.GetComponent<Comp_IInteractable>();
+        if(_IInteractable != null)
+        {
+            Debug.Log("Interaction Collision");
+            _IInteractable.InteractEnter();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _IInteractable = other.GetComponent<Comp_IInteractable>();
+        if(_IInteractable != null)
+        {
+            _IInteractable.InteractExite();
+        }
+    }
+}
