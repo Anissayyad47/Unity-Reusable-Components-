c616376a7754251d2fcb15fe08b9ee6b54d50282
ANIS SAYYAD <156597008+Anissayyad47@users.noreply.github.com>
2026-08-19 17:53:40 +0530

    Implemented interaction functionality in InteractableObject

--- Components/Interaction System/InteractableObject.cs
+++ Components/Interaction System/InteractableObject.cs
@@ -1,1 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Comp_Interaction : MonoBehaviour, IInteractable
+{
+    [Header("UI Reference")]
+    [SerializeField] private GameObject UI_Interact;
+    
+    public void InteractEnter(GameObject obj = null)
+    {
+        UI_Interact.SetActive(true);
+    }
+    public void Interact(GameObject obj = null)
+    {
+        // Action or Work
+    }
+    public void InteractExite()
+    {
+        UI_Interact.SetActive(false);
+    }
+}
