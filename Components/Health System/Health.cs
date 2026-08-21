373da56a6eedbcb651918dff142694fd624f3a6b
ANIS SAYYAD <156597008+Anissayyad47@users.noreply.github.com>
2026-08-21 18:26:25 +0530

    Add Comp_Health class for health management

---
+++ Components/Health System/Health.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comp_Health : MonoBehaviour
+{
+    [SerializeField] private HealthBarUI healthBar;
+    [SerializeField] private float maxHealth = 100f;
+
+    private float currentHealth;
+    public float CurrentHealth => currentHealth;
+    public float MaxHealth => maxHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(currentHealth <= 0 ) return;
+        currentHealth = Mathf.Max(currentHealth - damage, 0f);
+        healthBar.SetHealth(currentHealth);
+
+        if(currentHealth <= 0)
+        {
+            healthBar.gameObject.SetActive(false);
+            StartCoroutine(ObjectDisable());
+        }
+    }
+    IEnumerator ObjectDisable()
+    {
+        yield return new WaitForSeconds(1f);
+        gameObject.SetActive(false);
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth = Mathf.Min(currentHealth + amount, maxHealth);
+        healthBar.SetHealth(currentHealth);
+    }
+}
